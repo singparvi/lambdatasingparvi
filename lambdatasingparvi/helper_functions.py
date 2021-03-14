0997e750f043bbf0d431066c251267edb4cb4849
Parvi <sing.parvi1@gmail.com>
2021-03-14 14:31:18 -0400

    2021-03-14 Commit after making progress in assignment

--- lambdatasingparvi/helper_functions.py
+++ lambdatasingparvi/helper_functions.py
@@ -1,16 +1,14 @@
-
 import pandas as pd
 
 
 def null_count(df):
     print(df.isnull().sum().sum())
 
+
 def split_dates(date_series):
     month = date_series.dt.month
     day = date_series.dt.day
     year = date_series.dt.year
-    frame = { 'month': month, 'day': day, 'year': year }
+    frame = {"month": month, "day": day, "year": year}
     output = pd.DataFrame(frame)
     print(output)
-
-
